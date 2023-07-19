27a91cc2e140a52987895cd91c87689e3d23bdc0
Stevie Lapré <stevielapre@comaxx.nl>
2023-07-19 16:42:43 +0200

    Wijzigingen en koppelingen

--- forms.php
+++ forms.php
@@ -3,15 +3,32 @@
 <body>
 
 <?php
-echo $_SERVER['PHP_SELF'];
+$impressie = "";
+
+ if (empty($_POST["impressie"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["impressie"]);
+  }
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 ?>
 
-<form action="forms.php" method="post">
-    Name: <input type="text" name="name"><br>
-    E-mail: <input type="text" name="email"><br>
-    <input type="submit">
+<h1>Geef hier je oordeel over dit project</h1>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+Impressie: <textarea name="impressie" rows="2" cols="40"></textarea>
+    <input type="submit" name="submit" value="Submit">
 </form>
 
+<?php
+echo "<h2>Jouw ingevulde oordeel:</h2>";
+echo $impressie;
+echo "<br>";
+?>
 </body>
 </html>
 
