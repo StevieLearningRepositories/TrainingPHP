655d5c17bda5f0bb43137d999d2897d43ba4124f
Stevie Lapré <stevielapre@comaxx.nl>
2023-07-24 16:56:33 +0200

    Array gemaakt voor radio buttons

--- forms.php
+++ forms.php
@@ -10,9 +10,12 @@
     </style>
 </head>
 
+
 <?php
 $impressieErr = $improvementErr = $ratingErr = "";
 $impressie = $improvement = $rating = "";
+$numRate = 10;
+
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (empty($_POST["impressie"])) {
@@ -33,6 +36,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $improvement = test_input($_POST["improvement"]);
     }
 }
+
+
 function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
@@ -41,8 +46,10 @@ function test_input($data) {
 }
 ?>
 
+
 <h1>Geef hier je oordeel over dit project</h1>
 <p><span class="error">* Verplicht veld</span></p>
+
 
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     Impressie: <textarea name="impressie" rows="2" cols="40"></textarea>
@@ -50,22 +57,20 @@ function test_input($data) {
     Verbetering: <textarea name="improvement" rows="2" cols="40"></textarea>
     <span class="error">* <?php echo $improvementErr;?></span>
     <br><br>
-    Beoordeling:<input type="radio" name="rating" <?php if (isset($rating) && $rating==="1") echo "checked";?> value="1">1
-    <input type="radio" name="rating" <?php if (isset($rating) && $rating==="2") echo "checked";?> value="2">2
-    <input type="radio" name="rating" <?php if (isset($rating) && $rating==="3") echo "checked";?> value="3">3
-    <input type="radio" name="rating" <?php if (isset($rating) && $rating==="4") echo "checked";?> value="4">4
-    <input type="radio" name="rating" <?php if (isset($rating) && $rating==="5") echo "checked";?> value="5">5
-    <input type="radio" name="rating" <?php if (isset($rating) && $rating==="6") echo "checked";?> value="6">6
-    <input type="radio" name="rating" <?php if (isset($rating) && $rating==="7") echo "checked";?> value="7">7
-    <input type="radio" name="rating" <?php if (isset($rating) && $rating==="8") echo "checked";?> value="8">8
-    <input type="radio" name="rating" <?php if (isset($rating) && $rating==="9") echo "checked";?> value="9">9
-    <input type="radio" name="rating" <?php if (isset($rating) && $rating==="10") echo "checked";?> value="10">10
+
+
+    <?php for ($r = 1; $r <= $numRate; $r++) { ?>
+        <input type="radio" name="rating" value="<?= $r?>"
+            <?php if ($r === $rating) echo "checked"; ?> >
+        <?= $r?>
+            <?php } ?>
+
+
     <br>
     <span class="error">* <?php echo $ratingErr;?></span>
     <br><br>
     <input type="submit" name="submit" value="Verstuur">
 </form>
-
 
 
 <h2>Jouw ingevulde oordeel:</h2>
